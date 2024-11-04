9a705b6211a4797f2c3784cc00442fa748f3a04a
thetransientone <arth.vasavada@gmail.com>
2024-11-04 15:46:18 -0800

    Firebase Cloud Firestore initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-4xl text-blue-500 flex flex-col justify-self-center align-middle">Welcome to Aquazone</h1>
+    </>
+  )
+}
+
+export default App
